dbecb18e8c75809abf263dc2c217cb9bb276bfc1
Gabe Fierro <gtfierro225@gmail.com>
2016-06-17 14:50:47 -0700

    mroe accurate statisticos

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"time"
 
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 	bw "gopkg.in/immesys/bw2bind.v5"
 )
 
@@ -24,23 +26,32 @@ func main() {
 		svc.SetMetadata("description", os.Getenv("DESC"))
 	}
 	iface := svc.RegisterInterface("pantry", "i.top")
+	fmt.Println(iface.SignalURI("cpu"))
+	cl.SetMetadata(iface.SignalURI("cpu"), "SourceName", "top")
+	cl.SetMetadata(iface.SignalURI("mem"), "SourceName", "top")
+
+	cl.SetMetadata(iface.SignalURI("cpu"), "UnitofMeasure", "CPU")
+	cl.SetMetadata(iface.SignalURI("mem"), "UnitofMeasure", "MEM")
 
 	for {
-		usedC, totalC := getCpuUsage()
-		cpuPercentage := float64(usedC) / float64(totalC) * 100.0
+		cpus, err := cpu.Percent(3*time.Second, true)
+		cpuPercentage := float64(0)
+		for _, num := range cpus {
+			cpuPercentage += num
+		}
 		cpo, _ := bw.CreateMsgPackPayloadObject(bw.FromDotForm(bw.PODFMsgPack), map[string]interface{}{"cpu": cpuPercentage})
-		err := iface.PublishSignal("cpu", cpo)
+		err = iface.PublishSignal("cpu", cpo)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		usedM, totalM := getMemUsage()
-		mpo, _ := bw.CreateMsgPackPayloadObject(bw.FromDotForm(bw.PODFMsgPack), map[string]interface{}{"used": usedM / 1024 / 1024, "total": totalM / 1024 / 1024})
+		memstat, err := mem.VirtualMemory()
+		mpo, _ := bw.CreateMsgPackPayloadObject(bw.FromDotForm(bw.PODFMsgPack), map[string]interface{}{"used": memstat.UsedPercent})
 		err = iface.PublishSignal("mem", mpo)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Printf("cpu %v mem %v\n", cpuPercentage, usedM/1024/1024)
+		log.Printf("cpu %v mem %v\n", cpuPercentage, memstat.UsedPercent)
 
 		time.Sleep(3 * time.Second)
 	}
